e81206450afee97c2718b70aefb0864e7e802e92
Vadym Prudskyi <vadimprudskyi@gmail.com>
2026-08-31 23:29:07 +0200

    group structs logically

---
+++ target/debug/deps/mojang_api.d
@@ -0,0 +1,7 @@
+C:\Users\vadim\RustroverProjects\asymptote-launcher\target\debug\deps\mojang_api.d: src\main.rs src\core_prototype\mod.rs src\core_prototype\core.rs
+
+C:\Users\vadim\RustroverProjects\asymptote-launcher\target\debug\deps\mojang_api.exe: src\main.rs src\core_prototype\mod.rs src\core_prototype\core.rs
+
+src\main.rs:
+src\core_prototype\mod.rs:
+src\core_prototype\core.rs:
---
+++ target/debug/mojang-api.d
@@ -0,0 +1,1 @@
+C:\Users\vadim\RustroverProjects\asymptote-launcher\target\debug\mojang-api.exe: C:\Users\vadim\RustroverProjects\asymptote-launcher\src\core_prototype\core.rs C:\Users\vadim\RustroverProjects\asymptote-launcher\src\core_prototype\mod.rs C:\Users\vadim\RustroverProjects\asymptote-launcher\src\main.rs
